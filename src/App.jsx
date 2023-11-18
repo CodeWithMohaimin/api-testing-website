0639fd55163bbbf7c3e201ac4ee43433ce7dfeb7
CodeWithMohaimin <dev.mohaimin@gmail.com>
2023-11-18 18:10:25 +0600

    checking repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from "react";
-import Search from "./components/Search";
-
+import React from "react";
+import { useState, useEffect } from "react";
 const App = () => {
-  const searchInitValue = "phone";
-  const [posts, setPosts] = useState([]);
-  const [searchData, setSearchData] = useState(searchInitValue);
-
+  const [Data, setData] = useState([]);
   useEffect(() => {
-    const url = `https://amazon-api-b8fv.onrender.com/search/${searchData}`;
+    const header = {
+      Authorization: `Bearer${import.meta.VITE_REACT_API_BASE_URL}`,
+    };
+    const url = `http://localhost:1337/api/cards?populate=*`;
 
     const fetchData = async () => {
       try {
-        const response = await fetch(url);
+        const response = await fetch(url, { headers: header });
         const data = await response.json();
-        setPosts(data.results);
+        setData(data.data);
       } catch (error) {
         console.log("error", error);
       }
@@ -23,19 +22,23 @@ const App = () => {
   }, []);
 
   return (
-    <div>
-      <Search />
-      <div className="container">
-        {posts.map((post) => (
-          <div className="post" key={post.position}>
-            <img className="cover" src={post.image} alt="cover" />
-            <h1>{post.position}</h1>
-            <p>{post.name}</p>
-            <p>Ratings: {post.stars}</p>
-            <i>Price: {post.price_string}</i>
+    <div className="cardBody">
+      {Data.map((card) => (
+        <section className="card" key={card.id}>
+          <div>
+            <img
+              className="banner"
+              src={`http://localhost:1337${card.attributes.banner.data.attributes.url}`}
+              alt={card.attributes.banner.data.attributes.name}
+            />
           </div>
-        ))}
-      </div>
+          <div className="CardText">
+            <h1>{card.attributes.CardTitle}</h1>
+            <p>{card.attributes.description}</p>
+            <button>{card.attributes.button}</button>
+          </div>
+        </section>
+      ))}
     </div>
   );
 };
